92fe57b311b045ba062a750827df128d52208f00
Wojtek Stanisławski <stanik.woj@gmail.com>
2016-12-18 20:12:10 +0100

    costam jest

--- src/main/java/pl/pg/gda/eti/bioinfa/MainClass.java
+++ src/main/java/pl/pg/gda/eti/bioinfa/MainClass.java
@@ -6,8 +6,15 @@
 package pl.pg.gda.eti.bioinfa;
 
 import java.awt.BorderLayout;
+import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import org.biojava.bio.alignment.AlignmentAlgorithm;
+import org.biojava.bio.alignment.NeedlemanWunsch;
+import org.biojava.bio.alignment.SmithWaterman;
+import org.biojava.bio.alignment.SubstitutionMatrix;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.seq.Sequence;
 import org.biojava.bio.symbol.AlphabetManager;
 import org.biojava.bio.symbol.FiniteAlphabet;
 
@@ -16,24 +23,64 @@ import org.biojava.bio.symbol.FiniteAlphabet;
  * @author wojtek
  */
 public class MainClass {
-    
+
     public static void main(String[] args) {
-	
+	try {
+	    // The alphabet of the sequences. For this example DNA is choosen.
+	    FiniteAlphabet alphabet = (FiniteAlphabet) AlphabetManager.alphabetForName("DNA");
+	    // Read the substitution matrix file. 
+	    // For this example the matrix NUC.4.4 is good.
+	    SubstitutionMatrix matrix = new SubstitutionMatrix(alphabet, new File("/home/wojtek/NUC.4.4"));
+	    // Define the default costs for sequence manipulation for the global alignment.
+	    AlignmentAlgorithm aligner = new NeedlemanWunsch(
+		    (short) 0, // match
+		    (short) 3, // replace
+		    (short) 2, // insert
+		    (short) 2, // delete
+		    (short) 1, // gapExtend
+		    matrix // SubstitutionMatrix
+	    );
+	    Sequence query = DNATools.createDNASequence("ATAAGC", "query");
+	    Sequence target = DNATools.createDNASequence("AAAAACG", "target");
+	    // Perform an alignment and save the results.
+	    aligner.pairwiseAlignment(
+		    query, // first sequence
+		    target // second one
+	    );
+	    // Print the alignment to the screen
+	    System.out.println("Global alignment with Needleman-Wunsch:\n" + aligner.toString());
+
+	    // Perform a local alginment from the sequences with Smith-Waterman. 
+	    // Firstly, define the expenses (penalties) for every single operation.
+	    aligner = new SmithWaterman(
+		    (short) -1, // match
+		    (short) 3, // replace 
+		    (short) 2, // insert
+		    (short) 2, // delete
+		    (short) 1, // gapExtend
+		    matrix // SubstitutionMatrix
+	    );
+	    // Perform the local alignment.
+	    aligner.pairwiseAlignment(query, target);
+	    System.out.println("\nlocal alignment with SmithWaterman:\n" + aligner.toString());
+	} catch (Exception exc) {
+	    exc.printStackTrace();
+	}
+
 	JFrame frame = new JFrame();
-	
+
 	JLabel label = new JLabel("Hello World!");
-	
+
 	frame.setLayout(new BorderLayout());
 	frame.add(label, BorderLayout.CENTER);
 	frame.setSize(400, 300);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setLocationRelativeTo(null);
 	frame.setVisible(true);
-	
+
 	FiniteAlphabet alphabet = (FiniteAlphabet) AlphabetManager.alphabetForName("DNA");
 	System.out.println(alphabet);
-	
-	
+
     }
-    
+
 }
